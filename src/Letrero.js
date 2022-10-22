0002f2ccc138e382362bf8f04df0363de26e43b2
agrigaliunas <agustingrigaliunas@gmail.com>
2022-10-22 13:04:39 -0300

    TDD iteracion

--- src/Letrero.js
+++ src/Letrero.js
@@ -17,7 +17,13 @@ function Letrero() {
 
   this.apagarRectangulo = function (posicion1, posicion2) {};
 
-  this.encenderRectangulo = function (posicion1, posicion2) {};
+  this.encenderRectangulo = function (posicion1, posicion2) {
+    for (var i=posicion1.x ; i<=posicion2.x ; i++){
+        for(var j = posicion1.y ; j<=posicion2.y ; j++) {
+            this.luces[i][j].encender();
+        }
+    }
+  };
 
 
   this.estadoRectangulo = function (posicion1, posicion2) {
