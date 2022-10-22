cba61793eb57a19544598602cd850decf2daa616
agrigaliunas <agustingrigaliunas@gmail.com>
2022-10-22 13:09:18 -0300

    TDD iteracion

--- tests/coordenadas.test.js
+++ tests/coordenadas.test.js
@@ -5,7 +5,7 @@ test("La seccion del tablero [0,0]-[2,2] debe estar apagado",() =>{
     const letrero = new Letrero();
     const posicion1 = new Posicion(0,0);
     const posicion2 = new Posicion(2,2);
-
+    letrero.encenderRectangulo(posicion1,posicion2);
     letrero.apagarRectangulo(posicion1,posicion2);
     expect(letrero.estadoRectangulo(posicion1,posicion2)).toBe("Apagado");
 })
