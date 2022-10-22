7279d861a91eaeb76a00ef9c6a558c77da492f48
agrigaliunas <agustingrigaliunas@gmail.com>
2022-10-22 12:37:38 -0300

    TDD iteracion

--- tests/letrero.test.js
+++ tests/letrero.test.js
@@ -1,16 +1,18 @@
 const Letrero = require("../src/Letrero.js");
 const Posicion=require('../src/Posicion.js')
 
+beforeEach(()=> {
+    const letrero = new Letrero();
+    const posicion = new Posicion(1,2);
+});
+
+
 test("Letrero [1,2] encendido", () => {
-  const letrero = new Letrero();
-  const posicion = new Posicion(1,2);
   letrero.cambiarEstado(posicion);
   expect(letrero.estado(posicion)).toBe("Encendida");
 });
 
 test("Letrero [1,2] apagado", () => {
-  const letrero = new Letrero();
-  const posicion = new Posicion(1,2);
   expect(letrero.estado(posicion)).toBe("Apagada");
 });
 
