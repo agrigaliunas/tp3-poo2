931eb802e1de81fc020581635af4ca9f28db004a
agrigaliunas <agustingrigaliunas@gmail.com>
2022-10-22 12:02:23 -0300

    TDD iteracion

---
+++ src/Letrero.js
@@ -0,0 +1,5 @@
+function Letrero(){
+
+}
+
+module.exports=Letrero;
--- tests/letrero.test.js
+++ tests/letrero.test.js
@@ -1,4 +1,7 @@
+const Letrero= require('./Letrero.js')
+
 test("Letrero encendido", () => {
-    expect(letretro.estado).toBe("Encendido");
-  });
-  
+  expect(letretro.estado).toBe("Encendido");
+});
+
+// hacer refactor
