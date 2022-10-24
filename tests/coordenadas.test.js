f5128109ae6754363d58f3ba2345ac445353deaf
Agustin Grigaliunas <a.grigaliunas@ibm.com>
2022-10-24 17:11:43 -0300

    TDD iteracion

--- tests/coordenadas.test.js
+++ tests/coordenadas.test.js
@@ -26,6 +26,6 @@ test("La seccion del tablero [0,0]-[2,2] debe estar encendido",() =>{
 })
 
 test("Alterna luces del rectangulo [0,0]-[2,2] de encendido a apagado", () =>{
-    letreto.alternar();
+    letrero.alternar();
     expect(letreto.estadoRectangulo(posicion1,posicion2)).toBe("Apagado");
 })
