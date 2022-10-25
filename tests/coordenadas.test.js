aba943c335cd0bad861dfd3a56cf1a0f7a277272
agrigaliunas <agustingrigaliunas@gmail.com>
2022-10-24 21:22:36 -0300

    TDD iteracion

--- tests/coordenadas.test.js
+++ tests/coordenadas.test.js
@@ -25,12 +25,6 @@ test("La seccion del tablero [0,0]-[2,2] debe estar encendido",() =>{
     expect(letrero.estadoRectangulo(posicion1,posicion2)).toBe("Encendido");
 })
 
-test("Alterna luces del rectangulo [0,0]-[2,2] de encendido a apagado", () =>{
-    letrero.encenderRectangulo(posicion1,posicion2);
-    letrero.alternar(posicion1,posicion2);
-    expect(letrero.estadoRectangulo(posicion1,posicion2)).toBe("Apagado");
-})
-
 
 // 10000 luces
 test("Cuantas hay apagadas letrero", () =>{
